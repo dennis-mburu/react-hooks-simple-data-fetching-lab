1a0e0c836f86a615f7b4e9923632dfcec84d0ffd
Dennnis Mburu Mwaura <dennismburu2063@gmail.com>
2022-07-19 08:51:12 +0300

    Assignment Complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,23 @@
 // create your App component here
+import React, {useState, useEffect} from "react";
+
+function App(){
+    const[isLoaded, setIsLoaded] = useState(false)
+    const[dogImages, setDogImages] = useState([])
+
+    useEffect(()=>{
+        fetch('https://dog.ceo/api/breeds/image/random')
+        .then((res) => res.json())
+        .then((data) => {
+            setDogImages(data.message)
+            setIsLoaded(true)
+        })
+        }
+    ,[])
+
+
+if (!isLoaded) return <p>Loading...</p>
+return <img src={dogImages} alt="A Random Dog"/>
+}
+
+export default App;
